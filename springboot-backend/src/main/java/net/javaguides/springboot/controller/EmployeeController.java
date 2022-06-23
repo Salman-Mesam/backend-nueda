69f205b5201213607921693cff7a1fad827cddaf
Salman-Mesam <59708296+Salman-Mesam@users.noreply.github.com>
2022-06-23 20:40:22 +0000

    Life is beautiful

--- springboot-backend/src/main/java/net/javaguides/springboot/controller/EmployeeController.java
+++ springboot-backend/src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -20,7 +20,7 @@ import net.javaguides.springboot.exception.ResourceNotFoundException;
 import net.javaguides.springboot.model.Employee;
 import net.javaguides.springboot.repository.EmployeeRepository;
 
-@CrossOrigin(origins = "http://localhost:4200")
+
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
