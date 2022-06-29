159bfb2a72cfdb184ec2cfe0d7c710d15e54fb43
Salman-Mesam <59708296+Salman-Mesam@users.noreply.github.com>
2022-06-29 19:34:08 +0000

    Update SpringbootBackendApplicationTests.java

--- springboot-backend/src/test/java/net/javaguides/springboot/SpringbootBackendApplicationTests.java
+++ springboot-backend/src/test/java/net/javaguides/springboot/SpringbootBackendApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
-//salman mesam
+//salman
 @SpringBootTest
 class SpringbootBackendApplicationTests {
 
