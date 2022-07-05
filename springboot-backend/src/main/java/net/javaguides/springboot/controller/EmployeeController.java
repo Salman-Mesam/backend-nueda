08c27413633aa1124d5621412b8c28ce3b3e1367
Salman-Mesam <59708296+Salman-Mesam@users.noreply.github.com>
2022-07-05 17:25:37 +0000

    Update EmployeeController.java

--- springboot-backend/src/main/java/net/javaguides/springboot/controller/EmployeeController.java
+++ springboot-backend/src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -30,7 +30,7 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeRepository employeeRepository;
 	
-	// get all employees
+	// get all employees //
 	@GetMapping("/employees")
 	public List<Employee> getAllEmployees(){
 		return employeeRepository.findAll();
